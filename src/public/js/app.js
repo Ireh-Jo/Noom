cbf1d18460179d1357d5423b48ee2fb98603fb33
Ireh <kingslanding@naver.com>
2021-12-30 16:58:42 +0900

    Chat room done

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,7 +2,10 @@ const socket = io();
 
 const welcome = document.querySelector("#welcome");
 const join = document.querySelector("#join");
+const nickname = document.querySelector("#name");
 const room = document.querySelector("#room");
+const leaveBtn = document.querySelector(".room__leave");
+const roomList = document.querySelector("#roomList");
 
 room.hidden = true;
 
@@ -11,8 +14,10 @@ let roomName;
 function addMessage(message) {
     const ul = room.querySelector("ul");
     const li = document.createElement("li");
+    const aside = room.querySelector("section aside");
     li.innerText = message;
     ul.append(li);
+    aside.scrollTop = aside.scrollHeight;
 }
 
 function handleMessageSubmit(event) {
@@ -27,20 +32,40 @@ function handleMessageSubmit(event) {
 
 function showRoom(newCount) {
     welcome.hidden = true;
+    roomList.hidden = true;
     room.hidden = false;
     changeTitle(newCount);
     const msgForm = room.querySelector("#msg");
+    const ul = room.querySelector("section aside ul");
+    ul.innerHTML = "";
     msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
     event.preventDefault();
-    const roomInput = join.querySelector("input:nth-child(1)");
-    const nameInput = join.querySelector("input:nth-child(2)");
-    socket.emit("enter_room", roomInput.value, nameInput.value, showRoom);
+    const roomInput = join.querySelector("input");
+    socket.emit("enter_room", roomInput.value, showRoom);
     roomName = roomInput.value;
     roomInput.value = "";
-    nameInput.value = "";
+}
+
+function handleRoomClick(event) {
+    event.preventDefault();
+    const selectRoomName = event.target.innerText.split(" ")[0];
+    socket.emit("enter_room", selectRoomName, showRoom);
+    roomName = selectRoomName;
+}
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const nicknameInput = nickname.querySelector("input");
+    socket.emit("nickname_change", nicknameInput.value, changeNickname);
+    nicknameInput.value = "";
+}
+
+function handleLeaveBtnClick(event) {
+    event.preventDefault();
+    socket.emit("leave_room", roomName, leaveRoom);
 }
 
 function changeTitle(newCount) {
@@ -48,7 +73,20 @@ function changeTitle(newCount) {
     h3.innerText = `Room ${roomName} (${newCount})`;
 }
 
+function changeNickname(userName) {
+    const b = document.querySelector("header p b");
+    b.innerText = `'${userName}'`;
+}
+
+function leaveRoom() {
+    welcome.hidden = false;
+    roomList.hidden = false;
+    room.hidden = true;
+}
+
 join.addEventListener("submit", handleRoomSubmit);
+nickname.addEventListener("submit", handleNicknameSubmit);
+leaveBtn.addEventListener("click", handleLeaveBtnClick);
 
 socket.on("welcome", (user, newCount) => {
     changeTitle(newCount);
@@ -63,11 +101,21 @@ socket.on("bye", (user, newCount) => {
 socket.on("new_message", addMessage);
 
 socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
+    const openRoomList = document.querySelector("#roomList ul li ul");
+    const roomCount = document.querySelector("#roomList ul li a span");
+    roomCount.innerText = `(${rooms.length})`;
+    openRoomList.innerHTML = "";
     rooms.forEach((room) => {
         const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
+        const a = document.createElement("a");
+        a.href = "#";
+        a.innerText = room;
+        a.addEventListener("click", handleRoomClick);
+        li.append(a);
+        openRoomList.append(li);
     });
+});
+
+socket.on("nickname_change", (user) => {
+    changeNickname(user);
 });
